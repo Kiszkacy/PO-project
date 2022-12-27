e06bb21af12bec626227c3cbd0fba6921c113aec
MasterHamster42 <master_hamster9@protonmail.com>
2022-12-27 21:18:38 +0100

    fixes

--- src/main/java/evolution/maps/PlantMap.java
+++ src/main/java/evolution/maps/PlantMap.java
@@ -4,15 +4,34 @@ import evolution.events.Observer;
 import evolution.main.Map;
 import evolution.main.Plant;
 import evolution.util.Vector2;
-import java.util.LinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static evolution.util.EasyPrint.p;
 
 public abstract class PlantMap extends Map<Plant> implements Observer {
 
-    protected LinkedList<Vector2> preferredTiles;
-    protected LinkedList<Vector2> notPreferredTiles;
+    protected List<Vector2> preferredTiles;
+    protected List<Vector2> notPreferredTiles;
 
 
-    abstract void generatePreferredTiles();
+    public abstract void generatePreferredTiles();
+
+    public Plant newPlant(){
+        Random rd = new Random();
+        float probability = rd.nextFloat();
+        List<Vector2> chooseTiles;
+        if (probability > 0.2) chooseTiles = preferredTiles;
+        else chooseTiles = notPreferredTiles;
+        Vector2 plantPosition = chooseTiles.get(rd.nextInt(chooseTiles.size()));
+//        p(plantPosition);
+//        for (Vector2 vec : chooseTiles) {
+//            p(vec);
+//        }
+        return new Plant(plantPosition);
+    }
 
     // constructors
 
@@ -23,5 +42,15 @@ public abstract class PlantMap extends Map<Plant> implements Observer {
 
     public PlantMap(Vector2 size) {
         super(size);
+    }
+
+    // getters/ setters
+
+    public List<Vector2> getPreferredTiles() {
+        return preferredTiles;
+    }
+
+    public List<Vector2> getNotPreferredTiles() {
+        return notPreferredTiles;
     }
 }
--- src/main/java/evolution/maps/ToxicCorpses.java
+++ src/main/java/evolution/maps/ToxicCorpses.java
@@ -3,57 +3,178 @@ package evolution.maps;
 import evolution.events.DeathEvent;
 import evolution.events.DeathObserver;
 import evolution.events.Event;
-import evolution.events.MoveEvent;
 import evolution.main.Animal;
 import evolution.util.Vector2;
 
+import java.util.*;
+
+import static evolution.util.EasyPrint.p;
+
 public class ToxicCorpses extends PlantMap implements DeathObserver {
-    private int[][] deathMap;
+    private final int[][] deathMap;
+    private boolean checkValues = true;
+    private final int[] topValues;
 
     // constructors
 
     public ToxicCorpses(){
         super();
         this.deathMap = new int[this.size.y][this.size.x];
+        int amountOfTiles = size.y*size.x;
+        int preferredAmount = (int) Math.ceil(amountOfTiles * 0.2);
+        this.topValues = new int[preferredAmount];
+        this.initializePreferTiles(preferredAmount);
+//        // called in case someone tried to access preferredTiles, before calling generatePreferredTiles first time
+        this.notPreferredTiles = new ArrayList<>(Collections.nCopies(amountOfTiles - preferredAmount, (Vector2) null));
+        this.generatePreferredTiles();
     }
 
 
     public ToxicCorpses(Vector2 size){
         super(size);
         this.deathMap = new int[size.y][size.x];
+        int amountOfTiles = size.y*size.x;
+        int preferredAmount = (int) Math.ceil(amountOfTiles * 0.2);
+        this.topValues = new int[preferredAmount];
+        this.initializePreferTiles(preferredAmount);
+        // called in case someone tried to access preferredTiles, before calling generatePreferredTiles first time
+        this.notPreferredTiles = new ArrayList<>(Collections.nCopies(amountOfTiles - preferredAmount, (Vector2) null));
+        this.generatePreferredTiles();
+    }
+
+    /**
+     * helper function, used to initialize array preferredTiles, with starting values
+     * @param sizeOfPreferred size of array preferredTiles, equal to ceil of 0.2*amountOfTiles
+     */
+    private void initializePreferTiles(int sizeOfPreferred){
+        this.preferredTiles = new ArrayList<>(sizeOfPreferred);
+        for (int i = 0; i < sizeOfPreferred; i++) {
+            preferredTiles.add(i, new Vector2(i % size.x, i / size.x));
+        }
     }
 
     // overrides
 
-    // TODO return or save somewhere PreferredTiles
+    /**
+     * Generates preferred and notPreferred tiles, based on deathMap
+     */
     @Override
     public void generatePreferredTiles() {
-        int amountOFTiles = size.y*size.x;
-        // values are in increasing order (i.e 0 index is has smallest value)
-        int[] topValues = new int[(int) Math.ceil(amountOFTiles * 0.2)];
-        Vector2[] topValuesIndexes = new Vector2[(int) Math.ceil(amountOFTiles * 0.2)];
-        // can be optimized probably, for example using older topValues
+        // if no animal died since last generation of preferredTiles there is no need to update them
+        if (! checkValues) return;
+        // can be optimized probably, by checking only places where animal died since last check
 
-        for(int i = 0; i < size.y; i++){
-            for(int j = 0; j < size.x; j++){
+        // values are in increasing order (i.e 0 index has the smallest value)
+        int notPreferredIndex = 0;
 
-                int amountOfDeath = deathMap[i][j];
-                if (topValues[0] >= amountOfDeath) continue;
-                // insert new value in ordered manner
+        //updating topValues
+        preferredTiles.sort((v, u) -> deathMap[u.y][u.x] - deathMap[v.y][v.x]);
 
-                topValues[0] = amountOfDeath;
-                Vector2 position = new Vector2(j, i);
-                topValuesIndexes[0] = position;
+        for (int i = 0; i < preferredTiles.size(); i++) {
+            topValues[i] = deathMap[preferredTiles.get(i).y][preferredTiles.get(i).x];
+        }
 
-                for (int k = 1; k < topValues.length && amountOfDeath > topValues[k]; k++){
-                    topValues[k-1] = topValues[k];
-                    topValues[k] = amountOfDeath;
-                    topValuesIndexes[k-1] = topValuesIndexes[k];
-                    topValuesIndexes[k] = position;
+        for(int y = 0; y < size.y; y++){
+            for(int x = 0; x < size.x; x++){
+
+                int amountOfDeath = deathMap[y][x];
+                Vector2 position = new Vector2(x, y);
+
+                if (amountOfDeath > topValues[0]) {
+                    // value is smaller than min of topValues so goes to notPreferredTiles
+                    notPreferredTiles.set(notPreferredIndex, position);
+                    notPreferredIndex++;
                 }
+                else{
+                    // don't have to add element if it is already in list
+                    if (preferredTiles.contains(position)) continue;
+
+                    // if element isn't in list and is equal to min of list we add it to notPreferredTiles instead
+                    if(amountOfDeath == topValues[0]){
+                        notPreferredTiles.set(notPreferredIndex, position);
+                        notPreferredIndex++;
+                        continue;
+                        // if preferredTiles[0] is still to be iterated over, we don't add it to notPreferredTiles
+                    } else if ((preferredTiles.get(0).x <= x && preferredTiles.get(0).y <= y )|| preferredTiles.get(0).y < y) {
+                        notPreferredTiles.set(notPreferredIndex, preferredTiles.get(0));
+                        notPreferredIndex++;
+                    }
+
+                    topValues[0] = amountOfDeath;
+                    preferredTiles.set(0, position);
+
+                    // insert new value in ordered manner
+                    for (int k = 1; k < topValues.length && amountOfDeath < topValues[k]; k++) {
+                        topValues[k - 1] = topValues[k];
+                        topValues[k] = amountOfDeath;
+                        preferredTiles.set(k - 1, preferredTiles.get(k));
+                        preferredTiles.set(k, position);
+                    }
+                }
+
             }
         }
+        checkValues = false;
     }
+
+//    public void generatePreferredTiles() {
+//        // if no animal died since last generation of preferredTiles there is no need to update them
+//        if (! checkValues) return;
+//        // can be optimized probably, by checking only places where animal died since last check
+//
+//        // values are in increasing order (i.e 0 index has the smallest value)
+//        int notPreferredIndex = 0;
+//        ArrayList<Vector2> tmpPreferred = new ArrayList<>(preferredTiles.size());
+//
+//        //updating topValues
+////        preferredTiles.sort((u, v) -> deathMap[u.y][u.x] - deathMap[v.y][v.x]);
+//
+//        for (int i = 0; i < preferredTiles.size(); i++) {
+//            topValues[i] = deathMap[preferredTiles.get(i).y][preferredTiles.get(i).x];
+//        }
+//        int min = Arrays.stream(topValues).min().getAsInt();
+//
+//        for(int i = 0; i < size.y; i++){
+//            for(int j = 0; j < size.x; j++){
+//
+//                int amountOfDeath = deathMap[i][j];
+//                Vector2 position = new Vector2(j, i);
+//
+//                if (min > amountOfDeath) {
+//                    // value is smaller than min of topValues so goes to notPreferredTiles
+//                    notPreferredTiles.set(notPreferredIndex, position);
+//                    notPreferredIndex++;
+//                }
+//                else{
+////                    if (preferredTiles.contains(position)) continue;
+//
+//                    // don't have to add element if it is already in list
+//
+//                    // if element isn't in list and is equal to min of list we don't add it to preferredTiles
+////                    if(amountOfDeath == min) {
+////                        notPreferredTiles.set(notPreferredIndex, position);
+////                        notPreferredIndex++;
+////                        continue;
+////                    }
+//                        // if preferredTiles[0] is still to be iterated over, we don't add it to notPreferredTiles
+////                    } else if (preferredTiles.get(0).x <= j && preferredTiles.get(0).y <= i) {
+////                        notPreferredTiles.set(notPreferredIndex, preferredTiles.get(0));
+////                        notPreferredIndex++;
+////                    }
+//
+////                    topValues[0] = amountOfDeath;
+//                    tmpPreferred.add(position);
+//                }
+//
+//            }
+//        }
+//        tmpPreferred.sort((u, v) -> deathMap[u.y][u.x] - deathMap[v.y][v.x]);
+//        preferredTiles = tmpPreferred.subList(0, preferredTiles.size());
+//        for (int i = preferredTiles.size(); notPreferredIndex < notPreferredTiles.size() ; notPreferredIndex++, i++) {
+//            notPreferredTiles.set(notPreferredIndex, tmpPreferred.get(i));
+//        }
+//        checkValues = false;
+//    }
 
     /**
      * If event is death of animal, increment value of death map on index equal to its position
@@ -64,6 +185,7 @@ public class ToxicCorpses extends PlantMap implements DeathObserver {
         if (event.getDied() instanceof Animal){
             Vector2 position = ((Animal) event.getDied()).getPos();
             deathMap[position.y][position.x] += 1;
+            checkValues = true;
         }
     }
 
